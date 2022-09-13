b76152c5da729aca5bfa09614b981bed668a48c3
festad <d.festa004@studenti.unibs.it>
2022-09-14 01:57:34 +0200

    Factories

---
+++ database/factories/TransactionFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Transaction>
+ */
+class TransactionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $nusers = 10;
+	$sender_id = rand(1, $nusers-1);
+	$sender = User::findOrFail($sender_id);
+	$receiver_id = rand($sender_id+1, $nusers);
+	$receiver = User::findOrFail($receiver_id);
+	$amount = rand(10, 500);
+	
+        return [
+	    'sender' => $sender->pubkey,
+	    'receiver' => $receiver->pubkey,
+	    'amount' => $amount,
+        ];
+    }
+}
---
+++ database/factories/UserFactory.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class UserFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+
+        $email  = fake()->unique()->safeEmail();
+	$pubkey = hash('sha256', $email);
+	$pass   = Str::random(16);
+	$hash    = hash('sha256', $pass);
+	$balance = rand(10, 500);
+	
+        return [
+            'name' => fake()->name(),
+            'email' => $email,
+	    'pubkey' => $pubkey,
+            'email_verified_at' => now(),
+            'password' => $hash,
+	    'balance' => $balance,
+            'remember_token' => Str::random(10),
+        ];
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     *
+     * @return static
+     */
+    public function unverified()
+    {
+        return $this->state(fn (array $attributes) => [
+            'email_verified_at' => null,
+        ]);
+    }
+}
