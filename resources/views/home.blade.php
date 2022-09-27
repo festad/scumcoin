03888b81653b02d60d348cc5a332060fe2e62d2d
festad <d.festa004@studenti.unibs.it>
2022-09-28 00:11:16 +0200

    jquery home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -151,7 +151,8 @@
 
 
 	    <script
-		src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script> 
+		src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js">
+	    </script> 
 	    
 	    <script>
 	     $('body').css({
