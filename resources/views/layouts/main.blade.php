b8a3d8fa6bcccaa047547288434c96935834bd56
festad <d.festa004@studenti.unibs.it>
2024-03-04 01:38:34 +0100

    Templating homepage (external js file)

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -23,12 +23,67 @@
 
     <x-nav></x-nav> <!-- My archlinux-like navbar component -->
 
-    <div class="container">
-        @yield('content') <!-- Main content will be injected here -->
-    </div>
+    @yield('content') <!-- Main content will be injected here -->
 
     <!-- Scripts -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+
+
+    <!-- <script>
+        $('body').css({
+            'padding-top': '100px'
+        });
+
+        $('nav').css({
+            'border-bottom': '5px solid #08c',
+            'background-color': '#333333'
+        });
+
+        $('button').css({
+            'background-color': 'rgba(0,0,0,0)',
+            'border': '3px solid #08c',
+            'color': '#08c'
+        });
+
+        $('#login_button').css({
+            'margin-right': '10px'
+        });
+
+        $('#logo').on('mouseover', function() {
+            $(this).attr("src", "/s_circle_red.png");
+            $('.arch').css({
+                'background-color': 'red',
+                'border-color': 'red',
+                'color': 'white'
+            });
+        });
+
+        $('#logo').on('mouseout', function() {
+            $(this).attr("src", "/s_circle_blue.png");
+            $('.arch').css({
+                'background-color': 'rgba(0,0,0,0)',
+                'border-color': '#08c',
+                'color': '#08c'
+            });
+        });
+
+        $('button').on('mouseover', function() {
+            $(this).css({
+                'background-color': '#08c',
+                'border-color': '#08c',
+                'color': 'white'
+            });
+        });
+
+        $('button').on('mouseout', function() {
+            $(this).css({
+                'background-color': 'rgba(0,0,0,0)',
+                'border-color': '#08c',
+                'color': '#08c'
+            });
+        });
+    </script> -->
+    
 
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"
 		    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
